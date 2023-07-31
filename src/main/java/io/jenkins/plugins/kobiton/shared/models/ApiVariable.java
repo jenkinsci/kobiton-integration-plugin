4dda439fce2d54da82e4fe2d720f1f2f77880210
HuyTu <dragon10x0408@gmail.com>
2023-07-31 17:43:26 +0700

    Fix javadoc error and update codeowners

--- src/main/java/io/jenkins/plugins/kobiton/shared/models/ApiVariable.java
+++ src/main/java/io/jenkins/plugins/kobiton/shared/models/ApiVariable.java
@@ -2,9 +2,6 @@ package io.jenkins.plugins.kobiton.shared.models;
 
 import io.jenkins.plugins.kobiton.shared.utils.PropertyLoader;
 
-/**
- * Parameters for API requests & responses
- */
 public interface ApiVariable {
     String FILE_NAME = PropertyLoader.loadProperty("bodyFileName", "filename");
     String APP_ID = PropertyLoader.loadProperty("bodyAppId", "appId");
